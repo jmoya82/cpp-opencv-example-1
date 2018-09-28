5aa6655969a1dfe21012673723cbd64bedb4d5c1
Javier Moya <javier.moya@icloud.com>
2018-09-28 12:37:55 +0200

    Change project name

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 using namespace cv;
@@ -15,16 +16,20 @@ int main(int argc, const char * argv[]) {
     Mat image;
     image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     
-    if(! image.data )                              // Check for invalid input
+    if(! image.data )
     {
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
     
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    namedWindow( "Display window", WINDOW_AUTOSIZE );
     imshow( "Display window", image );
     
-    std::cout << "Hello, Worlderrr2!\n";
     waitKey(0);
+    
+    //image = cvtCvtColor(image, image, COLOR_RGB2GRAY);
+    
+    std::cout << "Hello, Worlderrr2!\n";
+    
     return 0;
 }
