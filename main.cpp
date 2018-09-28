b3ea7444b5fc6589f08e742cfdfc449f3bf35712
Javier Moya <javier.moya@icloud.com>
2018-09-28 09:37:28 +0200

    Add LZMA

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+using namespace cv;
+using namespace std;
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (argc !=2) {
+        cout << "Usage wrong" << endl;
+        return -1;
+    }
+    
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );
+    
+    std::cout << "Hello, Worlderrr2!\n";
+    waitKey(0);
     return 0;
 }
